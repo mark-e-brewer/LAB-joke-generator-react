fd2e485f2348c377a3c52b7ec79a7f337e68964b
Mark Brewer <markbrewer369@gmail.com>
2023-04-19 23:02:21 -0500

    aterm cards on dom and view term working. term form WIP

---
+++ api/termAPI.js
@@ -0,0 +1,79 @@
+import { clientCredentials } from '../utils/client';
+
+const dburl = clientCredentials.databaseURL;
+
+const getTerms = (uid) => new Promise((resolve, reject) => {
+  fetch(`${dburl}/term.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleTerm = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dburl}/term/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
+
+const createTerm = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dburl}/term.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTerm = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dburl}/term/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const deleteSingleTerm = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dburl}/term/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getTerms,
+  getSingleTerm,
+  createTerm,
+  updateTerm,
+  deleteSingleTerm,
+};
